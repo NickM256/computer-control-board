a9c70e011ecdd8948853fb8dcb325911a0b03877
NickM256 <nicholas.mineo@gmail.com>
2018-03-10 00:37:03 -0500

    Added blinking LED to Arduino

--- Arduino/IO-Manager/IO-Manager.ino
+++ Arduino/IO-Manager/IO-Manager.ino
@@ -1,18 +1,29 @@
-int incomingByte = 0;   // for incoming serial data
+#define LED_PIN 13  // Built-in led
+
+String incomingBytes = "AAA";   // for incoming serial data
 
 void setup() {
-        Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+  Serial.begin(9600);     // opens serial port, sets data rate to 9600 bps
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
+  // send data only when you receive data:
+  if (Serial.available() > 0) {
+      // read the incoming byte:
+    incomingBytes = Serial.readString();
+    flash(4);
+      // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incomingBytes);
+  }
+}
 
-        // send data only when you receive data:
-        if (Serial.available() > 0) {
-                // read the incoming byte:
-                incomingByte = Serial.read();
-
-                // say what you got:
-                Serial.print("I received: ");
-                Serial.println(incomingByte, DEC);
-        }
+void flash(int x){
+  for(int i = 0; i < x; i++){
+    digitalWrite(LED_PIN, HIGH);
+    delay(10);
+    digitalWrite(LED_PIN, LOW);
+    delay(100);
+  }
 }
